849411093ea999beb72420a4c777b998e782f3c0
victorabgdm <136820074+victorabgdm@users.noreply.github.com>
2024-02-21 15:44:23 +0000

    New project, it will be improved

---
+++ globalVars.py
@@ -0,0 +1,4 @@
+globalServer = 'DESKTOP-I5PNH1J\\SQLEXPRESS'
+globalDB = 'rvtech'
+
+connectionString = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={globalServer};DATABASE={globalDB};Trusted_Connection=yes;'
